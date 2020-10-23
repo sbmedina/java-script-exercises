67cd732b2e68ebce5038befd6aee2c795be9cbcd
Blas Medina <blasmedina5@gmail.com>
2020-10-23 20:48:45 -0300

    exercise 02

---
+++ 02-strings/app.js
@@ -0,0 +1,11 @@
+// exercise 02-a
+var text01 = 'so far so good';
+console.log (text01.toUpperCase() );
+// exercise 02-b
+var text02 = 'stop wishing start doing'
+var text03 = text02.substring (0,5)
+console.log (text03)
+// exercise 02-c
+var text04 = 'today is the day'
+var text05 = text04.substring (13,16)
+console.log (text05)
