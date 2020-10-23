d50065b6f7e3230e321047ff08320e10ae3caba7
Blas Medina <blasmedina5@gmail.com>
2020-10-23 20:21:12 -0300

    exercise 01

---
+++ 01-variables-y-operadores/app.js
@@ -0,0 +1,16 @@
+//exercice 01-a
+var num01 = 16;
+var num02 = 12.3;
+var total = num01 + num02
+console.log (total)
+//exercice 01-b
+var name = 'John '
+var lastName = 'Connor'
+var fullName = name += lastName
+console.log (fullName)
+//exercice 01-c
+var day = 'friday '
+var month = 'October'
+var date = day += month
+date.length
+console.log (date.length)
